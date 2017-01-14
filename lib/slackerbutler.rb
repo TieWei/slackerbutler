10360024a1d4200704d1789b77f5e96814852eb9
Wei Tie <wtie@cisco.com>
2017-01-13 19:47:08 -0800

    Init slackerbutler

---
+++ lib/slackerbutler.rb
@@ -0,0 +1,5 @@
+require 'slackerbutler/version'
+
+module Slackerbutler
+  # Your code goes here...
+end
---
+++ spec/slackerbutler_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Slackerbutler do
+  it 'has a version number' do
+    expect(Slackerbutler::VERSION).not_to be nil
+  end
+
+end
